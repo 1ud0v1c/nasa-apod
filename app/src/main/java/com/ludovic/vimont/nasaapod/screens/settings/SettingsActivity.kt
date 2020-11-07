5cdf605d6af93dde17f762b452fcbe8422a57bea
1ud0v1c <ludovic.vimont@gmail.com>
2020-11-07 22:24:19 +0100

    Prepare dark mode by adding theme action to SettingsActivity

--- app/src/main/java/com/ludovic/vimont/nasaapod/screens/settings/SettingsActivity.kt
+++ app/src/main/java/com/ludovic/vimont/nasaapod/screens/settings/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.ludovic.vimont.nasaapod.screens.settings
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NavUtils
 import com.ludovic.vimont.nasaapod.R
 import com.ludovic.vimont.nasaapod.databinding.ActivitySettingsBinding
 
@@ -13,10 +15,23 @@ class SettingsActivity : AppCompatActivity() {
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         title = getString(R.string.settings_activity_title)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
 
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.constraint_layout_container, SettingsFragment())
             .commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                NavUtils.navigateUpFromSameTask(this)
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
