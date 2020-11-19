f40f9a80a0a14b4609c960bf3e15782376522020
1ud0v1c <ludovic.vimont@gmail.com>
2020-11-19 19:10:39 +0100

    Fix SettingsFragment exit animation, temporary firebasefix & improve unit tests result display

--- app/src/main/java/com/ludovic/vimont/nasaapod/NasaApplication.kt
+++ app/src/main/java/com/ludovic/vimont/nasaapod/NasaApplication.kt
@@ -1,6 +1,7 @@
 package com.ludovic.vimont.nasaapod
 
 import android.app.Application
+import android.content.ServiceConnection
 import androidx.work.*
 import com.ludovic.vimont.nasaapod.background.worker.DailyRequestWorker
 import com.ludovic.vimont.nasaapod.background.worker.DailyRequestWorkerFactory
@@ -31,6 +32,16 @@ class NasaApplication: Application(), Configuration.Provider {
 
         val initialDelay: Long = TimeHelper.computeInitialDelay(9, 0)
         WorkerHelper.launchDailyWorker<DailyRequestWorker>(applicationContext, initialDelay)
+    }
+
+    // https://github.com/firebase/firebase-android-sdk/issues/1662
+    // https://www.bswen.com/2020/08/android-How-to-resolve-android-firebase-service-not-registered-exception.html
+    override fun unbindService(conn: ServiceConnection) {
+        try {
+            super.unbindService(conn)
+        } catch (e: IllegalArgumentException) {
+            println("IllegalArgumentException: ${e.message}")
+        }
     }
 
     override fun getWorkManagerConfiguration(): Configuration {
